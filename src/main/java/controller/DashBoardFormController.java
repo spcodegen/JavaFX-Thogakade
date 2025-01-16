7808f6b6e8c630281a0188ce2414ed7ff78b1902
spcodegen <shehansp2000@gmail.com>
2025-01-16 13:49:32 +0530

    Login UI

--- src/main/java/controller/DashBoardFormController.java
+++ src/main/java/controller/DashBoardFormController.java
@@ -13,7 +13,6 @@ public class DashBoardFormController {
     public AnchorPane loadFormContent;
 
     public void btnCustomerFormOnAction(ActionEvent actionEvent) throws IOException {
-
         URL resource = this.getClass().getResource("/view/customer_form.fxml");
 
         assert resource != null;
@@ -21,12 +20,21 @@ public class DashBoardFormController {
         Parent load = FXMLLoader.load(resource);
         loadFormContent.getChildren().clear();
         loadFormContent.getChildren().add(load);
-
     }
 
     public void btnItemFormOnAction(ActionEvent actionEvent) {
     }
 
     public void btnOrderFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRegisterFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = this.getClass().getResource("/view/register_form.fxml");
+
+        assert resource !=null;
+
+        Parent load=FXMLLoader.load(resource);
+        loadFormContent.getChildren().clear();
+        loadFormContent.getChildren().add(load);
     }
 }
