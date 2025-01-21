816843f68a30ac12ec12c46fba2b38d26884f01d
spcodegen <shehansp2000@gmail.com>
2025-01-21 13:11:23 +0530

    Order form and add date and time

--- src/main/java/controller/DashBoardFormController.java
+++ src/main/java/controller/DashBoardFormController.java
@@ -3,7 +3,9 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -25,7 +27,18 @@ public class DashBoardFormController {
     public void btnItemFormOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnOrderFormOnAction(ActionEvent actionEvent) {
+    public void btnOrderFormOnAction(ActionEvent actionEvent) throws IOException {
+//        Stage stage = new Stage();
+//        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/order_form.fxml"))));
+//        stage.show();
+
+        URL resource = this.getClass().getResource("/view/order_form.fxml");
+
+        assert resource!=null;
+        Parent load = FXMLLoader.load(resource);
+
+        loadFormContent.getChildren().clear();
+        loadFormContent.getChildren().add(load);
     }
 
     public void btnRegisterFormOnAction(ActionEvent actionEvent) throws IOException {
---
+++ src/main/java/controller/OrderFormController.java
@@ -0,0 +1,117 @@
+package controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.util.Duration;
+
+
+import java.net.URL;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class OrderFormController implements Initializable {
+
+    @FXML
+    private JFXComboBox cmbCustomerId;
+
+    @FXML
+    private JFXComboBox cmbItemCode;
+
+    @FXML
+    private TableColumn colDescription;
+
+    @FXML
+    private TableColumn colItemCode;
+
+    @FXML
+    private TableColumn colQtyOnHand;
+
+    @FXML
+    private TableColumn colTotal;
+
+    @FXML
+    private TableColumn colUnitPrice;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblNetTotal;
+
+    @FXML
+    private JFXTextField lblOrderId;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private TableView tblCart;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtDescription;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtPrice;
+
+    @FXML
+    private JFXTextField txtQty;
+
+    @FXML
+    private JFXTextField txtStock;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        getTimeAndTime();
+    }
+
+    @FXML
+    void btnAddToCardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
+
+    private void getTimeAndTime() {
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String format = dateFormat.format(date);
+        lblDate.setText(format);
+
+//-----------------------------------------------------------------------
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, e -> {
+                    LocalTime now = LocalTime.now();
+                    lblTime.setText(now.getHour()+":"+ now.getMinute()+":"+ now.getSecond());
+                }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
+
+    }
+
+
+}
